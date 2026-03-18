8e80ca6f1e8fb662478a4a3d605ef8246f92a455
bedlam343 <jagik22@gmail.com>
2026-03-18 15:50:50 -0700

    implement search page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -50,6 +50,8 @@ export default function Index() {
                         <SearchBar
                             onPress={() => router.push('/search')}
                             placeholder="Search for a movie"
+                            value=""
+                            onChangeText={() => {}}
                         />
 
                         <>
